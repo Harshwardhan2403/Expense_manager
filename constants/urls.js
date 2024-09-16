12bf265ca95f4334572d8308f0866431d2e96ba6
Harshwardhan Wandeshkar <112456163+Harshwardhan2403@users.noreply.github.com>
2024-09-16 16:14:57 +0530

    Create urls.js

---
+++ constants/urls.js
@@ -0,0 +1,8 @@
+const domain = "https://expensemanager20230425005316.azurewebsites.net/api/";
+const GetTransactions = `${domain}Transactions`;
+const GetCategories = `${domain}Categories`;
+
+export {
+    GetTransactions,
+    GetCategories,
+};
