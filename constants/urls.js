f684869837024fb5b8016059038aa194239ae6e0
Harshwardhan Wandeshkar <112456163+Harshwardhan2403@users.noreply.github.com>
2024-09-30 17:03:12 +0530

    Update urls.js

--- constants/urls.js
+++ constants/urls.js
@@ -2,6 +2,11 @@ const domain = "https://expensemanager20230425005316.azurewebsites.net/api/";
 const GetTransactions = `${domain}Transactions`;
 const GetCategories = `${domain}Categories`;
 
+
+
+
+
+
 export {
     GetTransactions,
     GetCategories,
